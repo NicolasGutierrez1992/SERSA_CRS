72ae87acfa95165b0dd7bdd54484d814ff2d46fc
Gustavo Nicolas Gutierrez <73355703+NicolasGutierrez1992@users.noreply.github.com>
2025-09-01 22:01:14 -0300

    add cors

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ require('dotenv').config({ override: true });
 const path = require('path');
 const authRouter = require('./server/auth'); // Asegúrate de que la ruta sea correcta
 const wscertRouter = require('./server/wscert');
+const cors = require('cors');
 
-
+const BACKEND_URL = 'https://sersa-crs.onrender.com';
 const app = express();
 const PORT = 3000;
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    BACKEND_URL // reemplaza por tu URL real de frontend en Render
+  ]
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); // Middleware para parsear JSON en las solicitudes
@@ -17,5 +25,5 @@ app.use('/api', wscertRouter );
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en ${BACKEND_URL}`);
 });
